5af6e5d17e09b0acd0c10b03df0f7e222495f959
Your Name <you@example.com>
2020-08-24 23:09:50 +0900

    add_image_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_125306) do
+ActiveRecord::Schema.define(version: 2020_08_24_134601) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_08_24_125306) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "images", "items"
 end
