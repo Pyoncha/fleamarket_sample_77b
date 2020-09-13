41eab0a3e606beb59e46f664b873d365addd96f8
tofuminto <tototofumifumin@gmail.com>
2020-09-13 11:13:25 +0900

    edit_seed_fu_migrate

--- Gemfile
+++ Gemfile
@@ -53,7 +53,6 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'faker'
   gem 'rails-controller-testing'
-  
 end
 
 group :development do
--- db/fixtures/development/2_image.rb
+++ db/fixtures/development/2_image.rb
@@ -4,31 +4,31 @@ FileUtils.rm(Dir.glob('*.*'))
 
 Image.seed do |s|
   s.id = 1
-  s.image = Rails.root.join("db/fixtures/images/a0003407_main.jpg").open
+  s.image = Rails.root.join("db/fixtures/development/images/a0003407_main.jpg").open
   s.item_id = 1
 end
 Image.seed do |s|
   s.id = 2
-  s.image = Rails.root.join("db/fixtures/images/ootoro_pic02.jpg").open
+  s.image = Rails.root.join("db/fixtures/development/images/ootoro_pic02.jpg").open
   s.item_id = 2
 end
 Image.seed do |s|
   s.id = 3
-  s.image = Rails.root.join("db/fixtures/images/footer_totop.png").open
+  s.image = Rails.root.join("db/fixtures/development/images/footer_totop.png").open
   s.item_id = 3
 end
 Image.seed do |s|
   s.id = 4
-  s.image = Rails.root.join("db/fixtures/images/battle-sec03_logo.png").open
+  s.image = Rails.root.join("db/fixtures/development/images/battle-sec03_logo.png").open
   s.item_id = 4
 end
 Image.seed do |s|
   s.id = 5
-  s.image = Rails.root.join("db/fixtures/images/1WftJJM1_400x400.jpg").open
+  s.image = Rails.root.join("db/fixtures/development/images/1WftJJM1_400x400.jpg").open
   s.item_id = 5
 end
 Image.seed do |s|
   s.id = 6
-  s.image = Rails.root.join("db/fixtures/images/ootoro_pic02.jpg").open
+  s.image = Rails.root.join("db/fixtures/development/images/ootoro_pic02.jpg").open
   s.item_id = 6
 end
