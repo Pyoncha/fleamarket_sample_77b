813e560bdcb896111f93dd452c88072d950b2c76
motti322 <hashimotoryosuke322@icloud.com>
2020-10-01 20:47:17 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users controllers: {
+
+  devise_for :users,controller:  {
     sessions: 'users/sessions',
     registrations: 'uers/registrations'
-  }
+   }
+
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'    
     post 'addresses', to: 'users/registrations#create_address'    
