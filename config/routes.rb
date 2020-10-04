a15e2b9e9c37fde585a5e0cecfed50e52392ad73
motti322 <hashimotoryosuke322@icloud.com>
2020-10-04 10:25:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-# コンフリクトが起きたのでコメントアウトしてます。（橋本）
-  ##devise_for :users,controller:  {
-   # sessions: 'users/sessions',
-    #registrations: 'users/registrations'
-    }
-# こちらも同じです
-  #devise_scope :user do
-    #get 'addresses', to: 'users/registrations#new_address'    
-    #post 'addresses', to: 'users/registrations#create_address'    
-    # get 'creditcards', to: 'users/registrations#new_creditcard'    
-    # post 'creditcards', to: 'users/registrations#create_creditcard'  
-  end
   root 'items#index'
   resources :items, only: [:index, :new, :create, :show] do
     get 'purchase'
